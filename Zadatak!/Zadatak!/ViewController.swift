d3fb29bd22c0046a3862ea3f87c0435a7a7bd3f4
이현욱 <hjhsha827@gmail.com>
2019-11-22 19:59:30 +0900

    UPDATE

---
+++ Zadatak!/Zadatak!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zadatak!
+//
+//  Created by 이현욱 on 2019/11/22.
+//  Copyright © 2019 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Zadatak/fontSize.swift
@@ -0,0 +1,14 @@
+//
+//  fontSize.swift
+//  Zadatak
+//
+//  Created by 이현욱 on 2019/11/22.
+//  Copyright © 2019 이현욱. All rights reserved.
+//
+
+import UIKit
+
+func fontSize22() {
+    let btn = UIButton()
+    btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: CGFloat(22))
+}
