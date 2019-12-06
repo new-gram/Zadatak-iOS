7bd890b37f429e520c9219347cd76a8834fd85ee
이현욱 <hjhsha827@gmail.com>
2019-12-06 17:24:17 +0900

    CREATE PROJECT

---
+++ Zadatak!/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Zadatak!
+//
+//  Created by 이현욱 on 2019/11/28.
+//  Copyright © 2019 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var listTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
